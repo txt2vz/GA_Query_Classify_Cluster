2a2be96b872225cc037423f1496a10eb9eb52cfa
txt2vz <lauriehirsch@gmail.com>
2016-11-03 15:20:00 +0000

    population size

--- GA_Query_Classify_Cluster/src/main/groovy/index/IndexInfo.groovy
+++ GA_Query_Classify_Cluster/src/main/groovy/index/IndexInfo.groovy
@@ -24,21 +24,20 @@ import org.apache.lucene.store.FSDirectory
 class IndexInfo {
 
 	// Lucene field names
-	public static final String FIELD_CATEGORY_NAME = 'category';
-	public static final String FIELD_CONTENTS = 'contents';
-	public static final String FIELD_PATH = 'path';
-	public static final String FIELD_TEST_TRAIN = 'test_train';
-	public static final String FIELD_CATEGORY_NUMBER = 'categoryNumber';
-	public static final int NUMBER_OF_CLUSTERS =  6, NUMBER_OF_CATEGORIES = 10
+	public static final String FIELD_CATEGORY_NAME = 'category',
+	                           FIELD_CONTENTS = 'contents', 
+							   FIELD_PATH = 'path',
+							   FIELD_TEST_TRAIN = 'test_train',
+							   FIELD_CATEGORY_NUMBER = 'categoryNumber';
+	public static final int NUMBER_OF_CLUSTERS =  4, NUMBER_OF_CATEGORIES = 10
 
 	String 	pathToIndex =
 	
-//	'indexes/crisis3FireBombFloodL5'
-	// 'indexes/classic4_500L5'
-	'indexes/20NG6GraphicsHockeyCryptSpaceChristianGunsL5'
+    // 'indexes/crisis3FireBombFloodL5'
+	 'indexes/classic4_500L5'
+	//'indexes/20NG6GraphicsHockeyCryptSpaceChristianGunsL5'
 	//'indexes/r10'
-    // "indexes/20NG3SpaceHockeyChristianl5"
-	//'indexes/20NG5WindowsMotorcycleSpaceMedMideast'
+    // 'indexes/20NG3SpaceHockeyChristianl5'
 
 	IndexReader indexReader
 	IndexSearcher indexSearcher
