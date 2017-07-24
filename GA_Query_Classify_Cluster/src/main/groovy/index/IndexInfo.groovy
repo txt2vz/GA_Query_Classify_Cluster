2ec9918a0b808bee275f2e6520a4f8a3e75637cf
txt2vz <lauriehirsch@gmail.com>
2017-07-24 10:31:15 +0100

    gradle group

--- GA_Query_Classify_Cluster/src/main/groovy/index/IndexInfo.groovy
+++ GA_Query_Classify_Cluster/src/main/groovy/index/IndexInfo.groovy
@@ -46,9 +46,9 @@ class IndexInfo {
 		            // new ClassicSimilarity()
 
 	static String pathToIndex =
-	   'indexes/R10'
+	 //  'indexes/R10'
 	//     'indexes/NG20'
-	//	 'indexes/crisis3FireBombFloodL6'
+		 'indexes/crisis3FireBombFloodL6'
 		// 'indexes/crisis3FireBombFloodL6.6Classic'
 	//	 'indexes/crisis3FireBombFloodL6.6'
 	// 'indexes/classic4_500L6'
